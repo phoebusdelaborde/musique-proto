09d8e218e3a345a9782a91d0c29f27b9c6ce0933
phoebusdelaborde <128900017+phoebusdelaborde@users.noreply.github.com>
2024-03-21 20:31:59 +0100

    vproto fin

---
+++ main.js
@@ -0,0 +1,14 @@
+let moyen1 = document.getElementById("moyen1");
+let moyen2 = document.getElementById("moyen2");
+let moyen3 = document.getElementById("moyen3");
+let moyen123 = document.getElementById("moyen123");
+
+moyen1.addEventListener("click", function () {
+  moyen123.play();
+});
+moyen2.addEventListener("click", function () {
+  moyen123.play();
+});
+moyen3.addEventListener("click", function () {
+  moyen123.play();
+});
