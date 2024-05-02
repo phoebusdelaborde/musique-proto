dfa7c2355ff5bb686ff8fbcdfec56b9c7dc8ab40
phoebusdelaborde <128900017+phoebusdelaborde@users.noreply.github.com>
2024-05-02 08:14:56 +0200

    v33

--- main.js
+++ main.js
@@ -1,14 +1,176 @@
-let moyen1 = document.getElementById("moyen1");
-let moyen2 = document.getElementById("moyen2");
-let moyen3 = document.getElementById("moyen3");
-let moyen123 = document.getElementById("moyen123");
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+let b4 = document.getElementById("b4");
+let b5 = document.getElementById("b5");
+let b6 = document.getElementById("b6");
+let b7 = document.getElementById("b7");
+let b8 = document.getElementById("b8");
+let b9 = document.getElementById("b9");
+let b10 = document.getElementById("b10");
+let b11 = document.getElementById("b11");
+let b12 = document.getElementById("b12");
+let b13 = document.getElementById("b13");
+let b14 = document.getElementById("b14");
+let b15 = document.getElementById("b15");
+let b16 = document.getElementById("b16");
+let b17 = document.getElementById("b17");
+let b18 = document.getElementById("b18");
+let b19 = document.getElementById("b19");
+let b20 = document.getElementById("b20");
+let b21 = document.getElementById("b21");
+let b22 = document.getElementById("b22");
+let b23 = document.getElementById("b23");
+let b24 = document.getElementById("b24");
+let b25 = document.getElementById("b25");
+let b26 = document.getElementById("b26");
+let b27 = document.getElementById("b27");
+let b28 = document.getElementById("b28");
+let b29 = document.getElementById("b29");
+let b30 = document.getElementById("b30");
+let b31 = document.getElementById("b31");
+let b32 = document.getElementById("b32");
+let b33 = document.getElementById("b33");
+let b34 = document.getElementById("b34");
+let b35 = document.getElementById("b35");
 
-moyen1.addEventListener("click", function () {
-  moyen123.play();
-});
-moyen2.addEventListener("click", function () {
-  moyen123.play();
-});
-moyen3.addEventListener("click", function () {
-  moyen123.play();
-});
+function a1() {
+  b1.play();
+
+  b2.stop();
+  b3.stop();
+  b4.stop();
+  b5.stop();
+  b6.stop();
+  b7.stop();
+  b8.stop();
+  b9.stop();
+  b10.stop();
+  b11.stop();
+  b12.stop();
+  b13.stop();
+  b14.stop();
+  b15.stop();
+  b16.stop();
+  b17.stop();
+  b18.stop();
+  b19.stop();
+  b20.stop();
+  b21.stop();
+  b22.stop();
+  b23.stop();
+  b24.stop();
+  b25.stop();
+  b26.stop();
+  b27.stop();
+  b28.stop();
+  b29.stop();
+  b30.stop();
+  b31.stop();
+  b32.stop();
+  b33.stop();
+  b34.stop();
+  b35.stop();
+}
+function a2() {
+  b2.play();
+}
+function a3() {
+  b3.play();
+}
+function a4() {
+  b4.play();
+}
+function a5() {
+  b5.play();
+}
+function a6() {
+  b6.play();
+}
+function a7() {
+  b7.play();
+}
+function a8() {
+  b8.play();
+}
+function a9() {
+  b9.play();
+}
+function a10() {
+  b10.play();
+}
+function a11() {
+  b11.play();
+}
+function a12() {
+  b12.play();
+}
+function a13() {
+  b13.play();
+}
+function a14() {
+  b14.play();
+}
+function a15() {
+  b15.play();
+}
+function a16() {
+  b16.play();
+}
+function a17() {
+  b17.play();
+}
+function a18() {
+  b18.play();
+}
+function a19() {
+  b19.play();
+}
+function a20() {
+  b20.play();
+}
+function a21() {
+  b21.play();
+}
+function a22() {
+  b22.play();
+}
+function a23() {
+  b23.play();
+}
+function a24() {
+  b24.play();
+}
+function a25() {
+  b25.play();
+}
+function a26() {
+  b26.play();
+}
+function a27() {
+  b27.play();
+}
+function a28() {
+  b28.play();
+}
+function a29() {
+  b29.play();
+}
+function a30() {
+  b30.play();
+}
+function a31() {
+  b31.play();
+}
+function a32() {
+  b32.play();
+}
+function a33() {
+  b33.play();
+}
+function a34() {
+  b34.play();
+}
+function a35() {
+  b35.play();
+}
